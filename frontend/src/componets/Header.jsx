1183ac808add6a546bce8b428459148130870f15
naveenprakashkv <naveen.prakash.kv@gmail.com>
2024-12-05 20:42:23 +0530

    header cart updated

--- frontend/src/componets/Header.jsx
+++ frontend/src/componets/Header.jsx
@@ -17,11 +17,21 @@ const Header = () => {
   const [category, setCategory] = useState("");
   const wishlist_count = 5;
   const { userInfo } = useSelector((store) => store.authUser);
+  const { cart_product_count } = useSelector((store) => store.cart);
   const { categories } = useSelector((store) => store.home);
+  // search onclick handle
   const search = () => {
     navigate(
       `/products/search?category=${category}&&searchValue=${searchValue}`
     );
+  };
+  // cart icon on click handle
+  const redirect_card_page = () => {
+    if (userInfo) {
+      navigate("/cart");
+    } else {
+      navigate("/login");
+    }
   };
   return (
     <div className="w-full bg-white relative">
@@ -37,7 +47,7 @@ const Header = () => {
           <div className="h-[80px] md-lg:[100px] flex justify-between items-center ">
             {/* logo */}
             <div className="flex justify-between items-center w-3/12 md-lg:w-full  ">
-              <Link>
+              <Link to="/">
                 <img
                   className="h-[60px]"
                   src="http://localhost:3000/images/logo.png"
@@ -56,20 +66,24 @@ const Header = () => {
             <div className="w-9/12 md-lg:w-full md-lg:hidden  ">
               <div className="">
                 <ul className="flex justify-center items-center gap-10 font-bold">
-                  <li
-                    className={`cursor-pointer px-3 py-2 rounded-md ${
-                      pathname === "/" ? "bg-blue-700 text-white" : ""
-                    } `}
-                  >
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li
-                    className={`cursor-pointer px-3 py-2 rounded-md ${
-                      pathname === "/shops" ? "bg-blue-700 text-white" : ""
-                    } `}
-                  >
-                    <Link to="/shops">Shop</Link>
-                  </li>
+                  <Link to="/">
+                    <li
+                      className={`cursor-pointer px-3 py-2 rounded-md ${
+                        pathname === "/" ? "bg-blue-700 text-white" : ""
+                      } `}
+                    >
+                      Home
+                    </li>
+                  </Link>
+                  <Link to="/shops">
+                    <li
+                      className={`cursor-pointer px-3 py-2 rounded-md ${
+                        pathname === "/shops" ? "bg-blue-700 text-white" : ""
+                      } `}
+                    >
+                      Shop
+                    </li>
+                  </Link>
                   <li
                     className={`cursor-pointer px-3 py-2 rounded-md ${
                       pathname === "/blog" ? "bg-blue-700 text-white" : ""
@@ -90,27 +104,25 @@ const Header = () => {
             {/* cart & wishlist */}
             <div className="md-lg:hidden flex justify-center items-center gap-6 ">
               {/* wishlist */}
-              <Link to="#">
-                <div className="relative">
-                  <span className="text-green-600 w-[30px] h-[30px] rounded-full bg-slate-200 flex justify-center items-center text-lg ">
-                    <FaHeart />
-                  </span>
-                  <div className="text-white text-[10px]  absolute w-[17px] h-[17px] -top-[5px] -right-[5px] bg-red-600 rounded-full flex justify-center items-center">
-                    {wishlist_count}
-                  </div>
+              <div className="relative">
+                <span className="text-green-600 w-[30px] cursor-pointer h-[30px] rounded-full bg-slate-200 flex justify-center items-center text-lg ">
+                  <FaHeart />
+                </span>
+                <div className="text-white text-[10px]  absolute w-[17px] h-[17px] -top-[5px] -right-[5px] bg-red-600 rounded-full flex justify-center items-center">
+                  {wishlist_count}
                 </div>
-              </Link>
+              </div>
               {/* cart*/}
-              <Link to="/cart">
-                <div className="relative">
-                  <span className="text-green-600 w-[30px] h-[30px] rounded-full bg-slate-200 flex justify-center items-center text-xl ">
-                    <MdShoppingCart />
-                  </span>
+              <div onClick={redirect_card_page} className="relative">
+                <span className="text-green-600 w-[30px] h-[30px] cursor-pointer rounded-full bg-slate-200 flex justify-center items-center text-xl ">
+                  <MdShoppingCart />
+                </span>
+                {cart_product_count !== 0 && (
                   <div className="text-white text-[10px]  absolute w-[17px] h-[17px] -top-[5px] -right-[5px] bg-red-600 rounded-full flex justify-center items-center">
-                    {wishlist_count}
+                    {cart_product_count}
                   </div>
-                </div>
-              </Link>
+                )}
+              </div>
             </div>
           </div>
         </div>
